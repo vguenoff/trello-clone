f077b7716ddbdcfc76861bf9ecb78bdb51dd4e4f
vguenoff <vguenoff@gmail.com>
2022-10-05 13:40:56 +0300

    Absolute path, folder restructure, scss, css modules

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import styles from '@/styles/App.module.scss'
+
+export default function App() {
+    return (
+        <div className={styles.App}>
+            <h1>Vite + React</h1>
+        </div>
+    )
+}
